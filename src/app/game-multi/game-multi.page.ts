13539764051dd37fbf7e6e7a0525db1d1592f9ca
selestino <selestinoolivier@gmail.com>
2026-08-29 14:35:45 +0300

    @selestinohajaniaina feat:Add a win&over modal

--- src/app/game-multi/game-multi.page.ts
+++ src/app/game-multi/game-multi.page.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Manche } from '../class/manche/manche';
-import { AlertController } from '@ionic/angular';
+import { AlertController, IonModal, ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { HttpService } from '../services/http.service';
 import { SocketService } from '../services/socket.service';
@@ -12,6 +12,8 @@ import { Subject, takeUntil } from 'rxjs';
   styleUrls: ['./game-multi.page.scss'],
 })
 export class GameMultiPage implements OnInit {
+  @ViewChild('modal') modal!: IonModal;
+
   public player!: 'X' | 'O';
   public playerName!: string;
   public opponentName!: string;
@@ -20,6 +22,7 @@ export class GameMultiPage implements OnInit {
   public nullScore: number = 0;
   public manche!: Manche;
   public theTour!: 'you' | 'opponent';
+  public status: 'win' | 'lose' | null = null;
   public board: ('X' | 'O' | null)[][] = [
     [null, null, null],
     [null, null, null],
@@ -30,9 +33,11 @@ export class GameMultiPage implements OnInit {
   private port!: number;
   private socketService!: SocketService;
   private destroy$ = new Subject<void>();
+  private audio = new Audio('assets/win.mp3');
 
   constructor(
     private alert: AlertController,
+    private toast: ToastController,
     private router: Router,
     private server: HttpService
   ) {}
@@ -61,8 +66,8 @@ export class GameMultiPage implements OnInit {
   }
 
   async choose(row: number, col: number) {
-    console.log("choose", row, col, this.board[row][col]);
-    
+    console.log('choose', row, col, this.board[row][col]);
+
     if (this.board[row][col] == null) {
       this.playerChoose(row, col);
     } else {
@@ -77,11 +82,11 @@ export class GameMultiPage implements OnInit {
   playerChoose(row: number, col: number) {
     if (this.theTour == 'opponent') return;
     this.theTour = 'you';
-    this.server.move(this.player, row, col, `${this.adress}:${this.port}`)
+    this.server
+      .move(this.player, row, col, `${this.adress}:${this.port}`)
       .subscribe((res) => {
-        console.log("res", res);
-        
-      })
+        console.log('res', res);
+      });
   }
 
   async leave() {
@@ -112,16 +117,20 @@ export class GameMultiPage implements OnInit {
     this.socketService.message$
       .pipe(takeUntil(this.destroy$))
       .subscribe((data) => {
-        const { board, players, log, currentPlayer } = data;
+        const { board, players, log, currentPlayer, winner } = data;
         const opponent = players[this.getOpponent(this.player)];
 
         if (opponent.connected) {
           this.opponentName = opponent.name;
         }
-
         this.board = board;
-
         this.theTour = currentPlayer == this.player ? 'you' : 'opponent';
+
+        if (winner) {
+          this.openModal();
+          this.status = this.player == winner ? 'win' : 'lose';
+          this.audio.play();
+        }
 
         console.log('State', data);
       });
@@ -130,5 +139,21 @@ export class GameMultiPage implements OnInit {
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  async openModal(): Promise<void> {
+    await this.modal.present();
+  }
+
+  async closeModal(): Promise<void> {
+    await this.modal.dismiss();
+  }
+
+  async showMessage(msg: string, title: string = '') {
+    const alert = await this.toast.create({
+      message: msg,
+      duration: 1500,
+    });
+    await alert.present();
   }
 }
